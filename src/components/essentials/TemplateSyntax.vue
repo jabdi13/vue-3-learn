022add9723d72f5360bdba9efb79969f87c1c32d
Abdiel Ortega <nathanabdiel1@gmail.com>
2023-12-03 04:14:47 -0600

    Raw HTML

--- src/components/essentials/TemplateSyntax.vue
+++ src/components/essentials/TemplateSyntax.vue
@@ -2,9 +2,13 @@
 import { ref } from 'vue';
 
   const msg = ref("Hello vue")
+  const rawHtml = '<span style="color: red">This should be red.</span>'
 </script>
 
 <template>
   <!-- Text interpolation -->
   <span>Message: {{ msg }}</span>
+  <!-- Raw HTML -->
+  <p>Using text interpolation: {{ rawHtml }}</p>
+  <p>Using v-html directive: <span v-html="rawHtml"></span></p>
 </template>
