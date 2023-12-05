e0aba49ed0b94805369e174d307f0c7e15c79f7f
Abdiel Ortega <nathanabdiel1@gmail.com>
2023-12-04 19:52:44 -0600

    Deep Reactivity

---
+++ src/components/essentials/2-ReactivityFundamentals/4-DeepReactivity/DeepReactivity.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    // In Vue, state is deeply reactive by default. This means you can expect changes to be detected even when you mutate nested objects or arrays
+    return {
+      obj: {
+        nested: { count: 0 },
+        arr: ['foo', 'bar']
+      }
+    }
+  },
+  methods: {
+    mutateDeeply() {
+      // these will work as expected.
+      this.obj.nested.count++
+      this.obj.arr.push('baz')
+    }
+  }
+}
+</script>
