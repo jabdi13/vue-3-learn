79c2a730d5306e2e1cb11a14d4aa9092aa00203c
Abdiel Ortega <nathanabdiel1@gmail.com>
2023-12-14 18:10:22 -0600

    Deep reactivity

---
+++ src/components/essentials/2-ReactivityFundamentals/composition-api/Unity2-DeepReactivity.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const obj = ref({
+    nested: {},
+    arr: ['foo', 'bar']
+  })
+
+  function mutateDeeply() {
+    // these will work as expected.
+    obj.value.nested.count++
+    obj.value.arr.push('baz')
+  }
+</script>
