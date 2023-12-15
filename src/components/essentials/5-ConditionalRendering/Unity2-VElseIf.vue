f519256ff500d15be2714345b1f4243a914d9d03
Abdiel Ortega <nathanabdiel1@gmail.com>
2023-12-15 14:38:44 -0600

    V else if

---
+++ src/components/essentials/5-ConditionalRendering/Unity2-VElseIf.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+
+  const showLetter = ref('C')
+</script>
+
+<template>
+  <div v-if="showLetter === 'A'">
+    A
+  </div>
+  <div v-else-if="showLetter === 'B'">
+    B
+  </div>
+  <div v-else-if="showLetter === 'C'">
+    C
+  </div>
+  <div v-else>
+    Not A/B/C
+  </div>
+</template>
