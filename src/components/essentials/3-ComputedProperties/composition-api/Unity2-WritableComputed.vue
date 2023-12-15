279f8a7a566281fae536fe4401c05c1dd8ed6970
Abdiel Ortega <nathanabdiel1@gmail.com>
2023-12-15 13:51:00 -0600

    Writable computed

---
+++ src/components/essentials/3-ComputedProperties/composition-api/Unity2-WritableComputed.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { computed, ref } from 'vue';
+
+  const firstName = ref('Abdiel')
+  const lastName = ref('Ortega')
+
+  const fullName = computed({
+    get() {
+      return `${firstName.value} ${lastName.value}`
+    },
+    set(newValue) {
+      [firstName.value, lastName.value] = newValue.split(' ')
+    }
+  })
+</script>
