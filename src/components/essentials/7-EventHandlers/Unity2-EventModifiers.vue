cef2ab21f6d04866c6be6d7bea038e46cc81be7f
Abdiel Ortega <nathanabdiel1@gmail.com>
2023-12-18 22:06:15 -0600

    Event modifiers

---
+++ src/components/essentials/7-EventHandlers/Unity2-EventModifiers.vue
@@ -0,0 +1,15 @@
+<script setup>
+function onSubmit() {
+  console.log("Prevented")
+}
+function submit() {
+  console.log("Key pressed")
+}
+</script>
+
+<template>
+  <form @submit.prevent="onSubmit">
+    <!-- only call `submit` when the `key` is `Enter` -->
+    <input @keyup.enter="submit" />
+  </form>
+</template>
