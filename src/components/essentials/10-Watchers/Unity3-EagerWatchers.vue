347dd14a4e68cac722fabcb443582112406c6cb0
Abdiel Ortega <nathanabdiel1@gmail.com>
2023-12-21 14:42:38 -0600

    Eager watchers

---
+++ src/components/essentials/10-Watchers/Unity3-EagerWatchers.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { watch } from 'vue';
+
+watch(
+  source,
+  (newValue, oldValue) => {
+    // executed immediately, then again when `source` changes
+  },
+  { immediate: true }
+)
+</script>
